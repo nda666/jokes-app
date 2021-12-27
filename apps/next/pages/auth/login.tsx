c44b904fff332006e782e684796c9d3fae0f8c9d
nda666 <adhabakhtiar@gmail.com>
2021-12-27 09:36:48 +0700

    add auth

---
+++ apps/next/pages/auth/login.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+export default function Login({ users, error }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      //   await auth.login(email, password);
+      setEmail('');
+      setPassword('');
+    } catch (e) {
+      console.error(e);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary" disabled={loading}>
+          Submit
+        </button>
+        {error && <p className="text-danger">{error.message}</p>}
+      </form>
+    </div>
+  );
+}
